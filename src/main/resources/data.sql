eef799593b8d1777ee94597f7b19da03bf6f59c2
HiroeIWMT <hiroeiwamoto1@gmail.com>
2024-07-12 23:14:10 +0200

    lesson80 modify error commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tasks(summary, description, status) VALUES ('Springを学ぶ','udemyの講座1','Aクラス');
-INSERT INTO tasks(summary, description, status) VALUES ('SpringSecurityを学ぶ','udemyの講座2','Bクラス');
-INSERT INTO tasks(summary, description, status) VALUES ('SpringInjectionを学ぶ','udemyの講座3','Aクラス');
-INSERT INTO tasks(summary, description, status) VALUES ('SpringDIを学ぶ','udemyの講座4','Cクラス');
+INSERT INTO tasks(summary, description, status) VALUES ('Springを学ぶ','udemyの講座1','TODO');
+INSERT INTO tasks(summary, description, status) VALUES ('SpringSecurityを学ぶ','udemyの講座2','DOING');
+INSERT INTO tasks(summary, description, status) VALUES ('SpringInjectionを学ぶ','udemyの講座3','DONE');
+INSERT INTO tasks(summary, description, status) VALUES ('SpringDIを学ぶ','udemyの講座4','TODO');
